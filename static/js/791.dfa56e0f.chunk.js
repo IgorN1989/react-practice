af7455a6acbb06290b2285772f26417e373db36d
IgorN1989 <107757685+IgorN1989@users.noreply.github.com>
2023-11-24 22:22:01 +0000

    Deploying to gh-pages from @ IgorN1989/react-practice@a78288f63798226569e26aec92a122c9d4020d7a 🚀

---
+++ static/js/791.dfa56e0f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{6791:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(1087),o=n(184);function a(){return(0,o.jsxs)("div",{children:["Page not found! Please go to ",(0,o.jsx)(r.rU,{to:"/",children:"Home Page"})]})}}}]);
+//# sourceMappingURL=791.dfa56e0f.chunk.js.map
